fd824d76ecd8319c7cb3d59c448f46d4e6f7e858
Farrah Ryan <ryanfarrah25@yahoo.fr>
2017-01-24 15:33:15 +0100

    PDO return base

--- model/pdo.php
+++ model/pdo.php
@@ -5,8 +5,6 @@ try
     $pdo = new PDO ('mysql:host=localhost;dbname=sarahr', 'sarahr', '86LQ2H2vkU');
 
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
 }
 
 catch (Exception $e)
