2165e74351350d75b5a3cd6e55da25ba6c6bb06a
Farrah Ryan <ryanfarrah25@yahoo.fr>
2017-01-24 15:30:15 +0100

    PDO improved

--- model/pdo.php
+++ model/pdo.php
@@ -3,8 +3,10 @@
 try
 {
     $pdo = new PDO ('mysql:host=localhost;dbname=sarahr', 'sarahr', '86LQ2H2vkU');
-    
+
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
 }
 
 catch (Exception $e)
